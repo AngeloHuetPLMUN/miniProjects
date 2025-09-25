d1bc3ea80e050557b63bd15806ec38a053ae7fbd
AngeloHuetPLMUN <huetangelo_bscs@plmun.edu.ph>
2025-09-25 23:26:49 +0800

    added lines, changed lines

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -8,7 +8,10 @@ def main():
     secret_number = random.randint(1, 100)
     attempts = 0
     MAX_ATTEMPTS = 10
-    print("You only have 10 attempts!", end="")
+    MIN = 1
+    MAX = 100
+
+    print("\nYou only have 10 attempts!", end="")
     while True:
         try:
             guess_number = int(input("\nGuess random number from 1-100: "))
@@ -16,8 +19,8 @@ def main():
             print("Please enter a valid number!")
             continue
 
-        if guess_number > 100 or guess_number < 1:
-            print("You cannot go higher than 100 and lower than 1!")
+        if guess_number > MAX or guess_number < MIN:
+            print("Number must be between 1 and 100! engot?")
             continue
 
         attempts += 1
@@ -27,20 +30,21 @@ def main():
         elif guess_number > secret_number:
             print("Too High, Try again.")
         else:
-            print(f"Nice You Won in {attempts} attempts!")
+            print(f"Nice! You Won in {attempts} attempts!")
             break
 
         if attempts == 5:
-            print(f"You have {attempts} attempts left!")
+            print(f"You have {MAX_ATTEMPTS - attempts} attempts left!")
         elif attempts == MAX_ATTEMPTS:
             print(
-                f"You used all {attempts} attempts you lose! 😭 tanga ka kase!")
+                f"You used all {attempts} attempts you lose! 😭 tanga ka kase!\n")
+            print(f"The secret number was {secret_number}.")
             break
 
 
 while True:
     main()
-    again = input("Play again? (y/n): ").lower()
+    again = input("\nPlay again? (y/n): ").lower()
     if again != "y":
         print("Thanks for playing! 🌵")
         break
